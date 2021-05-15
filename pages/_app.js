d8f63a0a9c1e0773fd0c5a2d422fe446e5741950
sidiDev <sididev3@gmail.com>
2021-05-15 23:18:38 +0000

    builded rest api

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,9 @@ function MyApp({ Component, pageProps }) {
   return (    
       <div className="app-container bg-gray-100">
         <Head>
+        <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="preconnect" href="https://media.nedigital.sg" />
           <link rel="dns-prefetch" href="https://media.nedigital.sg" />
           <meta name="google-site-verification" content="_AGAp-l5dDX_qosvctD2d6x0K_3-SKcPofPOd2sC1S4" />
