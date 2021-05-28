bfdbf1cc0896e8240af1b255b45781907951f97f
sidiDev <sididev3@gmail.com>
2021-05-28 23:12:38 +0000

    fix proplems

--- pages/components/Article/Article.js
+++ pages/components/Article/Article.js
@@ -7,7 +7,7 @@ import SocialMediaShare from '../SocialMediaShare/SocialMediaShare'
 
 const Article = (props) => {
 
-    const [article, setArticle] = useState(props.data.data)
+    // const [article, setArticle] = useState(props.data.data)
     const [loading, setLoading] = useState()
     const [shareToggle, setShareToggle]= useState(false)
 
@@ -26,9 +26,9 @@ const Article = (props) => {
         //     }
         // })
         
-        if (article.length > 0) {
+        if (props.data.data.length > 0) {
 
-            const convertToDOM = new DOMParser().parseFromString(article[0].article, "text/html")
+            const convertToDOM = new DOMParser().parseFromString(props.data.data[0].article, "text/html")
             articleContent.current.innerHTML = convertToDOM.childNodes[0].lastChild.childNodes[0].innerHTML
         }
 
@@ -45,27 +45,27 @@ const Article = (props) => {
                 //         </svg>
                 //     </div>
                 // ) : (
-                    article.length > 0 ? (
+                    props.data.data.length > 0 ? (
                         <div className="relative">
                             <Head>
-                                <meta property="og:image" content={`${article[0].img}?fit=max&amp;fm=jpg&amp;w=1000`} />
+                                <meta property="og:image" content={`${props.data.data.img}?fit=max&amp;fm=jpg&amp;w=1000`} />
                                 <meta property="og:image:width" content="1007" />
                                 <meta property="og:image:height" content="527" />
                                 <meta property="og:site_name" content="sidiDev - front end developer" />
                                 <meta name="keyword" content="programing,software,developer,blog,front end,front end job,web developer,web development,web designer,html,css,javascript,bootstrap,web dev,web,website, javascript,go,c#,c++,python,php,nodejs,expressjs,ruby,tailwindcss,sass,scss,reactjs,nextjs" />
-                                <meta name="description" content={article[0].description} />
+                                <meta name="description" content={props.data.data.description} />
                                 <meta name="twitter:site" content="@sidi_jeddou_dev" />
                                 <meta name="twitter:creator" content="@sidi_jeddou_dev" />
                                 <title>
-                                    {article[0].title}
+                                    {props.data.data[0].title}
                                 </title>
                             </Head>
                             <div className="relative">
-                                <img src={article[0].img} alt={article[0].title} className="w-full rounded-md h-60 sm:h-72 md:h-96" />
+                                <img src={props.data.data[0].img} alt={props.data.data[0].title} className="w-full rounded-md h-60 sm:h-72 md:h-96" />
                             
                                 <div className="absolute h-full right-4 top-0">
                                     <div className={`overflow-hidden absolute right-0 -bottom-1 w-12 ${shareToggle ? 'h-60' : 'h-0'} transform translate-y-2 shadow-md bg-white rounded-md rounded-br-lg`} style={{transition: '.3s'}}>
-                                        <SocialMediaShare url={'https://sidi.vercel.app/blog/'+router.query.url} title={article[0].title} />
+                                        <SocialMediaShare url={'https://sidi.vercel.app/blog/'+router.query.url} title={props.data.data[0].title} />
                                     </div>
                                     <button className="absolute -bottom-12 border right-0 transform -translate-y-6 shadow-md rounded-full w-12 h-12 text-indigo-600 bg-white flex items-center justify-center z-20 focus:outline-none focus:ring focus:ring-offset-indigo-600 focus:shadow-none"
                                         onClick={() => setShareToggle(!shareToggle)}
@@ -89,15 +89,15 @@ const Article = (props) => {
 
                             <div className="flex items-center space-x-3 mt-4 pt-3 pb-2">
                                 <div className="w-14 h-14 rounded-full">
-                                    <img src={'/images/'+article[0].createdLogo} className="w-14 h-14 rounded-full" alt={article[0].cratedName} />
+                                    <img src={'/images/'+props.data.data[0].createdLogo} className="w-14 h-14 rounded-full" alt={props.data.data[0].cratedName} />
                                 </div>
                                 <div>
-                                    <span className="block text-gray-700">Posted by <span className="font-medium text-indigo-700">{article[0].cratedName}</span></span>
-                                    <span className="block text-gray-700">Posted at <span className="font-medium text-indigo-700">{new Date(article[0].date).toDateString()}</span></span>
+                                    <span className="block text-gray-700">Posted by <span className="font-medium text-indigo-700">{props.data.data[0].cratedName}</span></span>
+                                    <span className="block text-gray-700">Posted at <span className="font-medium text-indigo-700">{new Date(props.data.data[0].date).toDateString()}</span></span>
                                 </div>
                             </div>
                             <h1 className="text-xl mt-4">
-                                {article[0].title}
+                                {props.data.data[0].title}
                             </h1>
                             <div className="mt-12 article-content" ref={articleContent}></div>
                         </div>
