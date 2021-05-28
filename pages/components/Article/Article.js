3d13ee0bec06541781431d85656258d87f3d92d3
sidiDev <sididev3@gmail.com>
2021-05-28 22:06:26 +0000

    fix proplems

--- pages/components/Article/Article.js
+++ pages/components/Article/Article.js
@@ -7,7 +7,7 @@ import SocialMediaShare from '../SocialMediaShare/SocialMediaShare'
 
 const Article = (props) => {
 
-    const [article, setArticle] = useState([props.data])
+    const [article, setArticle] = useState([props.data.data])
     const [loading, setLoading] = useState()
     const [shareToggle, setShareToggle]= useState(false)
 
