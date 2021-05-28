4e36e58d66a7707b9a30bd6e633f80df0c49f49a
sidiDev <sididev3@gmail.com>
2021-05-28 22:03:41 +0000

    fix proplems

--- pages/components/Article/Article.js
+++ pages/components/Article/Article.js
@@ -34,14 +34,14 @@ const Article = (props) => {
     return (
         <article className="mx-8 sm:m-auto sm:w-5/6 lg:w-4/6">
             {
-                loading ? (
-                    <div className="pb-12">
-                        <svg className="animate-spin m-auto h-14 w-14 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                        </svg>
-                    </div>
-                ) : (
+                // loading ? (
+                //     <div className="pb-12">
+                //         <svg className="animate-spin m-auto h-14 w-14 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                //             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                //             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                //         </svg>
+                //     </div>
+                // ) : (
                     article.length > 0 ? (
                         <div className="relative">
                             <Head>
@@ -99,7 +99,6 @@ const Article = (props) => {
                             <div className="mt-12 article-content" ref={articleContent}></div>
                         </div>
                     ) : ''
-                )
             }
         </article>
     )
