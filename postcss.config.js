4c88d91ff2b5ab8332b0d07a5670a19ac1210578
sidiDev <sididev3@gmail.com>
2021-07-26 15:12:07 +0000

    Little changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,30 @@
+// module.exports = {
+//     plugins: [
+//       "tailwindcss",
+//       [
+//         "@fullhuman/postcss-purgecss",
+//         process.env.NODE_ENV === "production"
+//           ? {
+//               // the paths to all template files
+//               content: [
+//                 "./pages/**/*.{js,jsx,ts,tsx}",
+//                 "./components/**/*.{js,jsx,ts,tsx}",
+//               ],
+//               // function used to extract class names from the templates
+//               defaultExtractor: (content) =>
+//                 content.match(/[\w-/:]+(?<!:)/g) || [],
+//             }
+//           : false,
+//       ],
+//     ],
+// };
+
 module.exports = {
-    plugins: [
-      "tailwindcss",
-      [
-        "@fullhuman/postcss-purgecss",
-        process.env.NODE_ENV === "production"
-          ? {
-              // the paths to all template files
-              content: [
-                "./pages/**/*.{js,jsx,ts,tsx}",
-                "./components/**/*.{js,jsx,ts,tsx}",
-              ],
-              // function used to extract class names from the templates
-              defaultExtractor: (content) =>
-                content.match(/[\w-/:]+(?<!:)/g) || [],
-            }
-          : false,
-      ],
-    ],
-  };
+  purge: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
